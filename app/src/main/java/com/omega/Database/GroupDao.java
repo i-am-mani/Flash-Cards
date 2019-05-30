aa5f90c879f59d6ccc11b325da8e72507ef347fb
Mandeep <mandeepch868@gmail.com>
2019-05-30 12:09:34 +0530

    Add CRUD operations on DAO for Group and FlashCard
    Expanded Dao Interface to include Update and delete operations.

--- app/src/main/java/com/omega/Database/GroupDao.java
+++ app/src/main/java/com/omega/Database/GroupDao.java
@@ -4,12 +4,16 @@ import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
 
 @Dao
 public interface GroupDao {
 
     @Insert
     public int insertGroup(Groups groups);
+
+    @Update
+    public void updateGroup(Groups group);
 
     @Query("SELECT * FROM Groups")
     public LiveData<Groups> getAllGroups();
