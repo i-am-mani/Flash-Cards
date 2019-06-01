652ac4dc0218fe65d6eaac52cc79bb23d5f90df8
Mandeep <mandeepch868@gmail.com>
2019-06-01 11:36:29 +0530

    Refactored Variable names

--- app/src/main/java/com/omega/Fragments/CreateFlashCardFragment.java
+++ app/src/main/java/com/omega/Fragments/CreateFlashCardFragment.java
@@ -2,13 +2,6 @@ package com.omega.Fragments;
 
 import android.app.Dialog;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.MutableLiveData;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,24 +10,26 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.omega.Adaptors.FlashCardsAdaptor;
-import com.omega.Database.Groups;
+import com.omega.R;
 import com.omega.Util.EqualSpaceItemDecoration;
 import com.omega.Util.FlashCardViewModel;
-import com.omega.R;
 
 public class CreateFlashCardFragment extends Fragment {
 
     private String GROUP_NAME;
-    private FlashCardsAdaptor adaptor;
-
-    private FloatingActionButton addFlashCard;
+    private final String TAG = CreateFlashCardFragment.class.getSimpleName();
+    private FlashCardsAdaptor rvAdaptor;
+    private FloatingActionButton btnAddFlashCard;
     private RecyclerView rvFlashCards;
-
-
-    final String TAG = "CreateFlashCardFragment";
     private FlashCardViewModel flashCardViewModel;
+
     public CreateFlashCardFragment(String groupName){
         GROUP_NAME  = groupName;
     }
@@ -43,13 +38,10 @@ public class CreateFlashCardFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         flashCardViewModel = ViewModelProviders.of(this).get(FlashCardViewModel.class);
+        //If Group Name is passed in constructor, attach the observer.
         if(GROUP_NAME != null){
             attachObserver();
         }
-    }
-
-    private void attachObserver() {
-        flashCardViewModel.getAllFlashCardsOfGroup(GROUP_NAME).observe(this,flashCards -> adaptor.setDataset(flashCards));
     }
 
     @Override
@@ -59,6 +51,54 @@ public class CreateFlashCardFragment extends Fragment {
         initializeInstanceVariables(viewGroup);
         initializeCallbacks(viewGroup);
         return viewGroup;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    private void initializeCallbacks(View viewGroup) {
+        Log.d(TAG, "initializeCallbacks: Creating new dialog");
+        btnAddFlashCard = viewGroup.findViewById(R.id.fab_create_flash_card);
+        btnAddFlashCard.setOnClickListener(v -> {
+            if (GROUP_NAME == null) {
+                initializeGroupDialog();
+            } else {
+                initializeFlashCardDialog();
+            }
+        });
+    }
+
+    private void initializeInstanceVariables(View viewGroup) {
+        rvAdaptor = new FlashCardsAdaptor(getActivity());
+        rvFlashCards = viewGroup.findViewById(R.id.recycler_view_flash_cards);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        rvFlashCards.setLayoutManager(layoutManager);
+        rvFlashCards.setAdapter(rvAdaptor);
+
+        rvFlashCards.addItemDecoration(new EqualSpaceItemDecoration(40));
+    }
+
+    private void initializeFlashCardDialog() {
+        Dialog dialog = new Dialog(getActivity());
+        dialog.setTitle("Create new Flash Card");
+        dialog.requestWindowFeature(Window.FEATURE_SWIPE_TO_DISMISS);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setContentView(R.layout.dailog_create_flash_card);
+
+        dialog.setOnShowListener(dialog1 -> {
+            Button btnCreateFlashCard = dialog.findViewById(R.id.button_create_flash_card);
+            btnCreateFlashCard.setOnClickListener(v1 -> {
+                String title = ((EditText) dialog.findViewById(R.id.edit_text_title)).getText().toString();
+                String content = ((EditText) dialog.findViewById(R.id.edit_text_message)).getText().toString();
+                Log.d(TAG, "initializeCallbacks: creating new flash card");
+                flashCardViewModel.createFlashCard(title, content, GROUP_NAME);
+                dialog.dismiss();
+            });
+        });
+
+        dialog.show();
     }
 
     private void initializeGroupDialog() {
@@ -85,52 +125,7 @@ public class CreateFlashCardFragment extends Fragment {
         dialog.show();
     }
 
-    public void initializeFlashCardDialog(){
-        Dialog dialog = new Dialog(getActivity());
-        dialog.setTitle("Create new Flash Card");
-        dialog.requestWindowFeature(Window.FEATURE_SWIPE_TO_DISMISS);
-        dialog.setCanceledOnTouchOutside(true);
-        dialog.setContentView(R.layout.dailog_create_flash_card);
-
-        dialog.setOnShowListener(dialog1 -> {
-            Button btnCreateFlashCard = dialog.findViewById(R.id.button_create_flash_card);
-            btnCreateFlashCard.setOnClickListener(v1 -> {
-                String title =((EditText) dialog.findViewById(R.id.edit_text_title)).getText().toString();
-                String content = ((EditText) dialog.findViewById(R.id.edit_text_message)).getText().toString();
-                Log.d(TAG, "initializeCallbacks: creating new flash card");
-                flashCardViewModel.createFlashCard(title,content,GROUP_NAME);
-                dialog.dismiss();
-            });
-        });
-
-        dialog.show();
-    }
-
-    private void initializeCallbacks(View viewGroup) {
-        Log.d(TAG, "initializeCallbacks: Creating new dialog");
-        addFlashCard = viewGroup.findViewById(R.id.fab_create_flash_card);
-        addFlashCard.setOnClickListener(v -> {
-            if (GROUP_NAME == null) {
-                initializeGroupDialog();
-            }
-            else{
-                initializeFlashCardDialog();
-            }
-        });
-    }
-
-    private void initializeInstanceVariables(View viewGroup) {
-        adaptor = new FlashCardsAdaptor(getActivity());
-        rvFlashCards = viewGroup.findViewById(R.id.recycler_view_flash_cards);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        rvFlashCards.setLayoutManager(layoutManager);
-        rvFlashCards.setAdapter(adaptor);
-
-        rvFlashCards.addItemDecoration(new EqualSpaceItemDecoration(40));
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
+    private void attachObserver() {
+        flashCardViewModel.getAllFlashCardsOfGroup(GROUP_NAME).observe(this, flashCards -> rvAdaptor.setDataset(flashCards));
     }
 }
