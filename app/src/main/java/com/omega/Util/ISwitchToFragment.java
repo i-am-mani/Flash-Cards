2924ae1900a13f72dd72a36c0176c4da8c9f1cf0
Mandeep <mandeepch868@gmail.com>
2019-06-01 11:09:07 +0530

    Move Fragment switching interface to separate file.

---
+++ app/src/main/java/com/omega/Util/ISwitchToFragment.java
@@ -0,0 +1,9 @@
+package com.omega.Util;
+
+import com.omega.Database.Groups;
+
+public interface ISwitchToFragment {
+    void switchToCreateFlashCard(Groups group);
+    void switchToOptions();
+    void switchToCheckoutFlashCard();
+}
