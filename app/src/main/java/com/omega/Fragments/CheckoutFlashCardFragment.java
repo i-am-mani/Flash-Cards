ff4e4dd8eddd7208adf6daac37affadd3dcab0b2
Mandeep <mandeepch868@gmail.com>
2019-06-04 19:45:17 +0530

    Implement edit item for groups on swipe

--- app/src/main/java/com/omega/Fragments/CheckoutFlashCardFragment.java
+++ app/src/main/java/com/omega/Fragments/CheckoutFlashCardFragment.java
@@ -1,11 +1,13 @@
 package com.omega.Fragments;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
@@ -13,6 +15,7 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.omega.Adaptors.GroupsAdaptor;
 import com.omega.Database.Groups;
 import com.omega.R;
@@ -26,6 +29,9 @@ public class CheckoutFlashCardFragment extends Fragment {
     FlashCardViewModel flashCardViewModel;
     GroupsAdaptor groupsAdaptor;
     ISwitchToFragment switchToFragment;
+
+    String newGroup;
+    String newDesc;
 
     String TAG = CheckoutFlashCardFragment.class.getSimpleName();
 
@@ -85,6 +91,26 @@ public class CheckoutFlashCardFragment extends Fragment {
             Groups group = groupsAdaptor.deleteRowFromDataSet(adapterPosition);
             flashCardViewModel.deleteGroup(group);
             Log.d(TAG, "deleteItem: Row Deleted");
+        }
+
+        @Override
+        public void editItem(int adapterPosition) {
+            Log.d(TAG, "editItem: Left Swiped");
+            String groupName = flashCardViewModel.getAllGroups().getValue().get(adapterPosition).getGroupName();
+
+            Dialog dialog = new Dialog(getActivity());
+            dialog.setCanceledOnTouchOutside(true);
+            dialog.setContentView(R.layout.dialog_edit_group);
+            dialog.getWindow().setBackgroundDrawableResource(R.color.DarkModePrimaryDarkColor);
+            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            Button btnConfirmEdit = dialog.findViewById(R.id.button_confirm_edit);
+            btnConfirmEdit.setOnClickListener(v -> {
+                newDesc = ((TextInputEditText) dialog.findViewById(R.id.edit_text_new_description)).getText().toString();
+                flashCardViewModel.updateGroup(groupName, newDesc);
+                dialog.dismiss();
+            });
+            dialog.setOnCancelListener(dialog1 -> groupsAdaptor.refresh(adapterPosition));
+            dialog.show();
         }
     }
 
