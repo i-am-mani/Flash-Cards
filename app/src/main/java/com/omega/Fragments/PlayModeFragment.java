d50505e605f034d145ee8e746fb870310fdb032c
Mandeep <mandeepch868@gmail.com>
2019-06-09 14:19:49 +0530

    Add timer which shows elapsed time

--- app/src/main/java/com/omega/Fragments/PlayModeFragment.java
+++ app/src/main/java/com/omega/Fragments/PlayModeFragment.java
@@ -2,6 +2,7 @@ package com.omega.Fragments;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,21 +27,39 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PlayModeFragment extends Fragment {
+
     private final static String KEY = "GroupName";
+    private static long START_TIME = 0;
     private String groupName;
+    @BindView(R.id.text_time)
+    TextView tvTime;
+    @BindView(R.id.text_score)
+    TextView tvScore;
+    Handler timerHandler = new Handler();
+
     @BindView(R.id.recycler_view_play_mode)
     RecyclerView rvPlayCard;
+
+    @BindView(R.id.text_play_mode_hint)
+    TextView tvHint;
+    Runnable timerRunnable = new Runnable() {
+        @Override
+        public void run() {
+            long millis = System.currentTimeMillis() - START_TIME;
+            int seconds = (int) (millis / 1000);
+            int minutes = seconds / 60;
+            seconds = seconds % 60;
+            tvTime.setText(String.format("%d:%02d", minutes, seconds));
+            timerHandler.postDelayed(this, 500);
+        }
+    };
     private PlayModeAdaptor playModeAdaptor;
     private FlashCardViewModel flashCardViewModel;
     private ISwitchToFragment ImplSwitchToFragment;
 
-    @BindView(R.id.text_play_mode_hint)
-    TextView tvHint;
-
     public PlayModeFragment() {
         //No Argument constructor
     }
-
 
     public PlayModeFragment(String groupName) {
         this.groupName = groupName;
@@ -83,6 +102,19 @@ public class PlayModeFragment extends Fragment {
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putString(KEY, groupName);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        START_TIME = START_TIME == 0 ? System.currentTimeMillis() : START_TIME;
+        tvTime.postDelayed(timerRunnable, 0);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        tvTime.removeCallbacks(timerRunnable);
     }
 
     private void initializeRecyclerView() {
