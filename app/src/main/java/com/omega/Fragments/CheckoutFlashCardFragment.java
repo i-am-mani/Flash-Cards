e56c00d880ddebe15173944ef78941b5ff6363ca
Mandeep <mandeepch868@gmail.com>
2019-06-09 15:18:50 +0530

    Refactored moved Inner classes outside of fragment class

--- app/src/main/java/com/omega/Fragments/CheckoutFlashCardFragment.java
+++ app/src/main/java/com/omega/Fragments/CheckoutFlashCardFragment.java
@@ -11,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -29,10 +28,12 @@ import com.omega.Util.EqualSpaceItemDecoration;
 import com.omega.Util.FlashCardViewModel;
 import com.omega.Util.ISwitchToFragment;
 import com.omega.Util.SwipeCallback;
+import com.omega.Util.Utility;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnTextChanged;
+
 
 public class CheckoutFlashCardFragment extends Fragment {
 
@@ -48,6 +49,9 @@ public class CheckoutFlashCardFragment extends Fragment {
 
     @BindView(R.id.edit_text_search)
     TextInputEditText et_search;
+
+    @BindView(R.id.recycler_view_group)
+    RecyclerView rvGroups;
 
     public CheckoutFlashCardFragment(){
 
@@ -74,32 +78,25 @@ public class CheckoutFlashCardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View viewGroup = inflater.inflate(R.layout.fragment_check_out_flash_cards, container, false);
         ButterKnife.bind(this, viewGroup);
-        getActivity().setTitle("Checkout Groups");
-        initializeVariables(viewGroup);
+        setActionBarTitle();
+        initialize();
         return viewGroup;
     }
 
-    private void initializeVariables(View viewGroup) {
-        RecyclerView rvGroups = viewGroup.findViewById(R.id.recycler_view_group);
-        groupsAdaptor = new GroupsAdaptor(getActivity(), new GroupsItemImpl() );
+    private void initialize() {
+        setUpRecyclerView();
+        setEditTextListeners();
+    }
+
+    private void setUpRecyclerView() {
+        groupsAdaptor = new GroupsAdaptor(getActivity(), new GroupsItemImpl(switchToFragment));
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
         rvGroups.setLayoutManager(linearLayoutManager);
         rvGroups.setAdapter(groupsAdaptor);
         rvGroups.addItemDecoration(new EqualSpaceItemDecoration(40));
 
-        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new SwipeCallback(new OnSwipeDeleteItem()));
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new SwipeCallback(new OnSwipeDeleteItem(groupsAdaptor, flashCardViewModel, getActivity())));
         itemTouchHelper.attachToRecyclerView(rvGroups);
-
-        et_search.setOnKeyListener((v, keyCode, event) -> {
-            Log.d(TAG, "initializeVariables: " + keyCode);
-            if (keyCode == KeyEvent.KEYCODE_ENTER) {
-                hideKeyboard(et_search);
-                et_search.setFocusable(false);
-                et_search.setFocusableInTouchMode(true);
-                return true;
-            }
-            return false;
-        });
     }
 
     @OnTextChanged(R.id.edit_text_search)
@@ -108,85 +105,136 @@ public class CheckoutFlashCardFragment extends Fragment {
         groupsAdaptor.filter(text.toString());
     }
 
-    public void hideKeyboard(View view) {
-        InputMethodManager inputMethodManager = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    private void setActionBarTitle() {
+        getActivity().setTitle("Checkout Groups");
     }
 
-    class GroupsItemImpl implements GroupsAdaptor.GroupsAdaptorListenerInterface {
-        @Override
-        public void onItemClick(View view,String groupName) {
-            switchToFragment.switchToCreateFlashCard(groupName);
-        }
-
-        @Override
-        public void onPlayButtonClicked(View view, String groupName) {
-            switchToFragment.switchToPlayMode(groupName);
-        }
+    private void setEditTextListeners() {
+        et_search.setOnKeyListener((v, keyCode, event) -> {
+            Log.d(TAG, "initialize: " + keyCode);
+            if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                Utility.hideKeyboard(et_search, getActivity());
+                et_search.setFocusable(false);
+                et_search.setFocusableInTouchMode(true);
+                return true;
+            }
+            return false;
+        });
     }
-
-    class OnSwipeDeleteItem implements SwipeCallback.OnSwiped {
-
-        @Override
-        public void deleteItem(int adapterPosition) {
-
-            Groups group = groupsAdaptor.getItemAtPosition(adapterPosition);
-
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
-            alertDialog.setTitle("Confirmation Alert");
-            alertDialog.setMessage("Are you sure you want to delete item ? ");
-
-            alertDialog.setPositiveButton("Yes", (dialog, which) -> {
-                flashCardViewModel.deleteGroup(group);
-            });
-            alertDialog.setNegativeButton("Undo", (dialog, which) -> {
-                groupsAdaptor.refresh(adapterPosition);
-            });
-
-            alertDialog.setOnCancelListener(dialog -> groupsAdaptor.refresh(adapterPosition));
-
-            AlertDialog dialog = alertDialog.create();
-            dialog.getWindow().setBackgroundDrawableResource(R.color.DarkModePrimaryDarkColor);
-            dialog.show();
-        }
-
-        @Override
-        public void editItem(int adapterPosition) {
-            Log.d(TAG, "editItem: Left Swiped");
-            Groups group = groupsAdaptor.getItemAtPosition(adapterPosition);
-
-            Dialog dialog = new Dialog(getActivity());
-            dialog.requestWindowFeature(Window.FEATURE_SWIPE_TO_DISMISS);
-            dialog.setCanceledOnTouchOutside(true);
-            dialog.setContentView(R.layout.dialog_edit);
-
-            ((TextView) (dialog.findViewById(R.id.text_dialog_title))).setText("Edit Group");
-
-            TextInputEditText etDesc = dialog.findViewById(R.id.edit_text_new_description);
-            TextInputEditText etName = dialog.findViewById(R.id.edit_text_new_group_name);
-
-            etName.setText(group.getGroupName());
-            etDesc.setText(group.getGroupDescription());
-
-            dialog.getWindow().setBackgroundDrawableResource(R.color.DarkModePrimaryColor);
-            dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-            Button btnConfirmEdit = dialog.findViewById(R.id.button_confirm_edit);
-            btnConfirmEdit.setOnClickListener(v -> {
-                newDesc = etDesc.getText().toString();
-                newName = etName.getText().toString();
-
-                group.setGroupDescription(newDesc);
-                group.setGroupName(newName);
-
-                flashCardViewModel.updateGroup(group);
-                dialog.dismiss();
-                groupsAdaptor.refresh(adapterPosition);
-            });
-            dialog.setOnCancelListener(dialog1 -> groupsAdaptor.refresh(adapterPosition));
-            dialog.show();
-        }
-    }
-
 
 }
 
+class GroupsItemImpl implements GroupsAdaptor.GroupsAdaptorListenerInterface {
+
+    private ISwitchToFragment switchToFragment;
+
+    GroupsItemImpl(ISwitchToFragment implSwitchToFragment) {
+        switchToFragment = implSwitchToFragment;
+    }
+
+    @Override
+    public void onItemClick(View view, String groupName) {
+        switchToFragment.switchToCreateFlashCard(groupName);
+    }
+
+    @Override
+    public void onPlayButtonClicked(View view, String groupName) {
+        switchToFragment.switchToPlayMode(groupName);
+    }
+}
+
+
+class OnSwipeDeleteItem implements SwipeCallback.OnSwiped {
+
+    private GroupsAdaptor groupsAdaptor;
+    private FlashCardViewModel flashCardViewModel;
+    private Activity activity;
+    private String newDesc;
+    private String newName;
+
+    public OnSwipeDeleteItem(GroupsAdaptor adaptor, FlashCardViewModel viewModel, Activity activity) {
+        groupsAdaptor = adaptor;
+        flashCardViewModel = viewModel;
+        this.activity = activity;
+    }
+
+    @Override
+    public void deleteItem(int adapterPosition) {
+
+        Groups group = groupsAdaptor.getItemAtPosition(adapterPosition);
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(activity);
+        alertDialog.setTitle("Confirmation Alert");
+        alertDialog.setMessage("Are you sure you want to delete item ? ");
+
+        alertDialog.setPositiveButton("Yes", (dialog, which) -> {
+            flashCardViewModel.deleteGroup(group);
+        });
+        alertDialog.setNegativeButton("Undo", (dialog, which) -> {
+            groupsAdaptor.refresh(adapterPosition);
+        });
+
+        alertDialog.setOnCancelListener(dialog -> groupsAdaptor.refresh(adapterPosition));
+
+        AlertDialog dialog = alertDialog.create();
+        dialog.getWindow().setBackgroundDrawableResource(R.color.DarkModePrimaryDarkColor);
+        dialog.show();
+    }
+
+    @Override
+    public void editItem(int adapterPosition) {
+        Groups group = groupsAdaptor.getItemAtPosition(adapterPosition);
+
+        Dialog dialog = getDialog();
+
+        // fill the existing info
+        TextInputEditText etDesc = dialog.findViewById(R.id.edit_text_new_description);
+        TextInputEditText etName = dialog.findViewById(R.id.edit_text_new_group_name);
+        autoFillDialogFields(group, etDesc, etName);
+
+        // set dialog's window properties
+        setDialogWindow(dialog);
+
+        // set button click actions
+        Button btnConfirmEdit = dialog.findViewById(R.id.button_confirm_edit);
+        setUpButtonClickListeners(adapterPosition, group, dialog, etDesc, etName, btnConfirmEdit);
+        dialog.show();
+    }
+
+    private void autoFillDialogFields(Groups group, TextInputEditText etDesc, TextInputEditText etName) {
+        etName.setText(group.getGroupName());
+        etDesc.setText(group.getGroupDescription());
+    }
+
+    private void setDialogWindow(Dialog dialog) {
+        dialog.getWindow().setBackgroundDrawableResource(R.color.DarkModePrimaryColor);
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    private void setUpButtonClickListeners(int adapterPosition, Groups group, Dialog dialog, TextInputEditText etDesc, TextInputEditText etName, Button btnConfirmEdit) {
+        btnConfirmEdit.setOnClickListener(v -> {
+            newDesc = etDesc.getText().toString();
+            newName = etName.getText().toString();
+
+            group.setGroupDescription(newDesc);
+            group.setGroupName(newName);
+
+            flashCardViewModel.updateGroup(group);
+            dialog.dismiss();
+            groupsAdaptor.refresh(adapterPosition);
+        });
+
+        dialog.setOnCancelListener(dialog1 -> groupsAdaptor.refresh(adapterPosition));
+    }
+
+    private Dialog getDialog() {
+        Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_SWIPE_TO_DISMISS);
+        dialog.setCanceledOnTouchOutside(true);
+        dialog.setContentView(R.layout.dialog_edit);
+
+        ((TextView) (dialog.findViewById(R.id.text_dialog_title))).setText("Edit Group");
+        return dialog;
+    }
+
+}
