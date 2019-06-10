57b1e0aeb0ecc9ab2277999471ee50bf764cf066
Mandeep <mandeepch868@gmail.com>
2019-06-10 12:07:27 +0530

    Add Switch methods for new play modes

--- app/src/main/java/com/omega/Util/ISwitchToFragment.java
+++ app/src/main/java/com/omega/Util/ISwitchToFragment.java
@@ -2,7 +2,12 @@ package com.omega.Util;
 
 public interface ISwitchToFragment {
     void switchToCreateFlashCard(String group);
-
     void switchToPlayMode(String group);
     void switchToCheckoutFlashCard();
+
+    void switchToPlayModeCasual(String group);
+
+    void switchToPlayModeReverseMatch(String group);
+
+    void switchToPlayModeTrueFalse(String group);
 }
