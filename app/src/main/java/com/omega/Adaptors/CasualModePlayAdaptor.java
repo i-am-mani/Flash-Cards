8a990c3c65e4793f2c65d0003653ca6c1d8d3d0f
Mandeep <mandeepch868@gmail.com>
2019-06-21 19:52:40 +0530

    Add Card number EditText and Shuffle button

--- app/src/main/java/com/omega/Adaptors/CasualModePlayAdaptor.java
+++ app/src/main/java/com/omega/Adaptors/CasualModePlayAdaptor.java
@@ -2,9 +2,16 @@ package com.omega.Adaptors;
 
 import android.content.Context;
 
+import java.util.Collections;
+
 public class CasualModePlayAdaptor extends SimpleFlashCardViewerAdapter {
 
     public CasualModePlayAdaptor(Context context) {
         super(context);
+    }
+
+    public void shuffle() {
+        Collections.shuffle(dataSet);
+        notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/omega/Fragments/CasualPlayModeFragment.java
+++ app/src/main/java/com/omega/Fragments/CasualPlayModeFragment.java
@@ -2,9 +2,11 @@ package com.omega.Fragments;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,17 +21,26 @@ import com.omega.Adaptors.CasualModePlayAdaptor;
 import com.omega.R;
 import com.omega.Util.EqualSpaceItemDecoration;
 import com.omega.Util.FlashCardViewModel;
+import com.omega.Util.Utility;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class CasualPlayModeFragment extends Fragment {
 
+    private static String GROUP_NAME;  // Since it's static it would persists across Orientation Change
+
     @BindView(R.id.recycler_view_casual_mode_flash_cards)
     RecyclerView rvCasualPlay;
-    private static String GROUP_NAME;
+    private final String AdaptorPositionKey = "AdapterPositionKey";
+    @BindView(R.id.edit_text_flashcard_number)
+    EditText etFlashCardNumber;
+
     private CasualModePlayAdaptor casualModePlayAdaptor;
     private FlashCardViewModel flashCardViewModel;
+    LinearLayoutManager linearLayoutManager;
+    private int adaptorItemPosition = 0; // holds position of adaptor item currently visible
 
     public CasualPlayModeFragment() {
         //No-arg constructor
@@ -44,17 +55,24 @@ public class CasualPlayModeFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         flashCardViewModel = ViewModelProviders.of(this).get(FlashCardViewModel.class);
         flashCardViewModel.getAllFlashCardsOfGroup(GROUP_NAME).observe(this, flashCards -> {
-//            if (savedInstanceState != null) {
-//                useSavedInstanceState();
-//            } else {
-            casualModePlayAdaptor.setDataSet(flashCards);
-//            }
+            if (savedInstanceState != null) {
+                useSavedInstanceState(savedInstanceState);
+            } else {
+                if (flashCards.size() > 0) {
+                    casualModePlayAdaptor.setDataSet(flashCards);
+                }
+            }
         });
     }
-//
-//    private void useSavedInstanceState() {
-//        casualModePlayAdaptor.set
-//    }
+
+    private void useSavedInstanceState(Bundle savedInstanceState) {
+        adaptorItemPosition = savedInstanceState.getInt(AdaptorPositionKey, adaptorItemPosition);
+        scrollRecyclerView(adaptorItemPosition);
+    }
+
+    private void scrollRecyclerView(int itemPosition) {
+        rvCasualPlay.scrollToPosition(itemPosition);
+    }
 
     @Nullable
     @Override
@@ -63,13 +81,33 @@ public class CasualPlayModeFragment extends Fragment {
         ButterKnife.bind(this, mainView);
         initializeVariables();
         getActivity().setTitle("Casual Mode");
+        setUpEditText();
         return mainView;
+    }
+
+    private void setUpEditText() {
+        Utility.onReturnKeyEditText(etFlashCardNumber, getActivity());
+        etFlashCardNumber.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    Utility.hideKeyboard(etFlashCardNumber, getActivity());
+                    etFlashCardNumber.setFocusable(false);
+                    etFlashCardNumber.setFocusableInTouchMode(true);
+
+                    int pos = Integer.valueOf(etFlashCardNumber.getText().toString());
+                    rvCasualPlay.scrollToPosition(getAdaptorItemPosition(pos));
+                    return true;
+                }
+                return false;
+            }
+        });
     }
 
     @Override
     public void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putString("GroupName", GROUP_NAME);
+        outState.putInt(AdaptorPositionKey, adaptorItemPosition);
     }
 
     private void initializeVariables() {
@@ -79,7 +117,7 @@ public class CasualPlayModeFragment extends Fragment {
     private void initRecyclerView() {
         Log.d("TAG", "initRecyclerView: " + rvCasualPlay);
         casualModePlayAdaptor = new CasualModePlayAdaptor(getActivity());
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false);
+        linearLayoutManager = new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false);
 
         rvCasualPlay.setAdapter(casualModePlayAdaptor);
         rvCasualPlay.setLayoutManager(linearLayoutManager);
@@ -89,6 +127,32 @@ public class CasualPlayModeFragment extends Fragment {
 
         SnapHelper snapHelper = new LinearSnapHelper();
         snapHelper.attachToRecyclerView(rvCasualPlay);
+
+        rvCasualPlay.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                int visibleItemPosition = linearLayoutManager.findFirstCompletelyVisibleItemPosition();
+                if (visibleItemPosition != -1) {
+                    adaptorItemPosition = getDisplayPosition(visibleItemPosition);
+                    etFlashCardNumber.setText(String.valueOf(adaptorItemPosition));
+                }
+            }
+        });
+    }
+
+    @OnClick(R.id.button_shuffle)
+    public void shuffleCards(View view) {
+        casualModePlayAdaptor.shuffle();
+        scrollRecyclerView(getAdaptorItemPosition(0));
+    }
+
+    private int getDisplayPosition(int pos) {
+        return pos + 1;
+    }
+
+    private int getAdaptorItemPosition(int pos) {
+        return pos - 1;
     }
 }
 
